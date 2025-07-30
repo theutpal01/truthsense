cc9ae22d07f81359916cefc3135df303eb711018
Utpal <dev.theutpal@gmail.com>
2025-07-30 21:39:55 +0530

    fix: linting issues

--- apps/frontend/src/app/feedback/[id]/page.tsx
+++ apps/frontend/src/app/feedback/[id]/page.tsx
@@ -1,13 +1,10 @@
-import { AuthGuard } from '@/services/auth-guard';
 import Report from './ReportClient';
-import { type FC } from 'react';
 
-const ReportPage: FC<{ params: { id: string } }> = ({ params }) => {
-	return (
-		<AuthGuard>
-			<Report id={params.id} />
-		</AuthGuard>
-	);
-};
-
-export default ReportPage;
+export default async function ReportPage({
+	params,
+}: {
+	params: Promise<{ id: string }>
+}) {
+	const { id } = await params;
+	return <Report id={id} />;
+}
